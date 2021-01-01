f3279cad6100b2a24b7026c28da447d6870a462e
Viditv68 <viditrawatv68@gmail.com>
2021-01-01 10:42:48 +0530

    Move within Range

---
+++ Assets/Scripts/Player/Mover.cs
@@ -0,0 +1,51 @@
+﻿using RPG.Combat;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        private NavMeshAgent nav;
+        private Animator animator;
+
+        private void Start()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = nav.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+
+            animator.SetFloat("forwardSpeed", speed);
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+
+            GetComponent<Fighter>().CancelAttack();
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            nav.isStopped = false;
+            nav.destination = destination;
+            
+        }
+
+        public void Stop()
+        {
+            nav.isStopped = true;
+        }
+    }
+}
