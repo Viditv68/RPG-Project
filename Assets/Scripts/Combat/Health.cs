6dc34f2facaacf4080b477ae77edbc35d2126d1b
Viditv68 <viditrawatv68@gmail.com>
2021-01-04 11:37:29 +0530

    Attack

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField]
+        private float health = 100;
+
+        private bool isDead = false;
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max((health - damage), 0);
+            Debug.Log(health);
+
+            if(health ==0 && !isDead)
+            {
+                isDead = true;
+                GetComponent<Animator>().SetTrigger("die");
+            }
+        }
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
+    }
+}
